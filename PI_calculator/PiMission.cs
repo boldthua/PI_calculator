283f06cd38d922010f45ef39c970c7d410a8137e
LAPTOP-R67HGS5E\User <boldthua0330@gmail.com>
2026-01-23 11:06:38 +0800

    0123菜

---
+++ PI_calculator/PiMission.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_calculator
+{
+    internal class PiMission
+    {
+        public readonly long SampleSize;
+        public double Calculate()
+        {
+            int sum = 0;
+            List<double> list = new List<double>();
+            Random random = new Random();
+
+            for (int i = 0; i < SampleSize; i++)
+            {
+                Double a = (long)random.NextDouble();
+                Double b = (long)random.NextDouble();
+                if (a * a + b * b <= 1)
+                    sum++;
+            }
+            return sum / SampleSize - 1;
+
+        }
+        public PiMission(long SampleSize)
+        {
+            this.SampleSize = SampleSize;
+        }
+    }
+}
